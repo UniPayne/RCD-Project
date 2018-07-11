57bae3b80e0401c4937a6019a1fe2c8b6422abfc
Payne <sean.payne@b-next.com>
2018-07-11 11:21:12 +0200

    1

--- src/scoring_helper/src/Komponenten/Uebersicht.js
+++ src/scoring_helper/src/Komponenten/Uebersicht.js
@@ -27,44 +27,8 @@ class Uebersicht extends React.Component {
       heimTeam_name:            '',
       austragungsort:           '',
       zuschauer:                '',
-
-      gast_SpielerRNummer:      '',
-      gast_SpielerNName:        '',
-      gast_SpielerVName:        '',
-      gast_SpielerPosition:     '',
-      gast_SpielerPNummer:      '',
-
-      showGastPopup:            false,
-      showInfoPopup:            false
     };
 
-}
-
-toggleGastPopup(){
-  this.setState({showGastPopup: !this.state.showGastPopup});
-}
-
-toggleInfoPopup() {
-  this.setState({showInfoPopup: !this.state.showInfoPopup});
-}
-
-submitSpielinfosMain(stati){
-  this.setState({spielnummer:     stati.form_spielnummer});
-  this.setState({spieldatum:      stati.form_spieldatum});
-  this.setState({liga_name:       stati.form_liga_name});
-  this.setState({verband_name:    stati.form_verband_name});
-  this.setState({gastTeam_name:   stati.form_gastTeam_name});
-  this.setState({heimTeam_name:   stati.form_heimTeam_name});
-  this.setState({austragungsort:  stati.form_austragungsort});
-  this.setState({zuschauer:       stati.form_zuschauer});
-}
-
-submitGastMain(stati){
-  this.setState({gast_SpielerRNummer:   stati.form_gast_SpielerRNummer});
-  this.setState({gast_SpielerNName:     stati.form_gast_SpielerNName});
-  this.setState({gast_SpielerVName:     stati.form_gast_SpielerVName});
-  this.setState({gast_SpielerPosition:  stati.form_gast_SpielerPosition});
-  this.setState({gast_SpielerPNummer:   stati.form_gast_SpielerPNummer});
 }
 
 
