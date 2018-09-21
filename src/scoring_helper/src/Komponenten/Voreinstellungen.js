a4ff83c4f6de173004b9fdf9f166c12e8728e7c4
Payne <sean.payne@b-next.com>
2018-09-21 14:48:07 +0200

    Voreinstellungen komplett fertig
    1

--- src/scoring_helper/src/Komponenten/Voreinstellungen.js
+++ src/scoring_helper/src/Komponenten/Voreinstellungen.js
@@ -89,7 +89,6 @@ constructor(props){
       {spielerRNummer: "29", spielerNName: "29", spielerVName: "29", spielerPosition: "29", spielerPNummer: "29"}
     ],
     heim_ok:                false,
-    alles_ok: false,
   }
 }
 // <-- bis hier!
@@ -121,19 +120,6 @@ submitSpielinfosMain(stati){
   this.setState({info_ok:         stati.form_info},() => {
   console.log("Voreinstellungen Info_ok: ",this.state.info_ok);
   } );
-  if (this.state.info_ok === true &&
-      this.state.gast_ok === true &&
-      this.state.heim_ok === true){
-        this.setState({alles_ok: true}, () => {
-          console.log("alles:ok: ", this.state.alles_ok);
-        });
-      }
-      else {
-        this.setState({alles_ok: false}, () => {
-          console.log("alles:ok: ", this.state.alles_ok);
-      });
-}
-
 }
 
 
@@ -144,18 +130,6 @@ submitGastMain(stati){
   this.setState({gast_spieler: stati.spielerArray}, () => {
      console.log(this.state.gast_spieler);
    });
-   if (this.state.info_ok === true &&
-       this.state.gast_ok === true &&
-       this.state.heim_ok === true){
-         this.setState({alles_ok: true}, () => {
-           console.log("alles:ok: ", this.state.alles_ok);
-         });
-       }
-       else {
-         this.setState({alles_ok: false}, () => {
-           console.log("alles:ok: ", this.state.alles_ok);
-       });
- }
 }
 
 
@@ -166,18 +140,6 @@ submitHeimMain(stati){
   this.setState({heim_spieler: stati.spielerArray}, () => {
      console.log(this.state.heim_spieler);
    });
-   if (this.state.info_ok === true &&
-       this.state.gast_ok === true &&
-       this.state.heim_ok === true){
-         this.setState({alles_ok: true}, () => {
-           console.log("alles:ok: ", this.state.alles_ok);
-         });
-       }
-       else {
-         this.setState({alles_ok: false}, () => {
-           console.log("alles:ok: ", this.state.alles_ok);
-       });
- }
 }
 
 
@@ -368,7 +330,7 @@ submitHeimMain(stati){
 				  pathname: '/Uebersicht',
 				  spielinfos: this.state
 			  }}>
-                <button disabled={!this.state.alles_ok}className="vor_button_weiter">Spiel Starten</button>
+                <button disabled={!(this.state.info_ok && this.state.gast_ok && this.state.heim_ok) }className="vor_button_weiter">Spiel Starten</button>
               </Link>
             </div>
           </div>
