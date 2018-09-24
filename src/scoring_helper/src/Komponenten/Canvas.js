4e443ef53f6d3e72b2cc36a6c33717c24ebe0c1e
Payne <sean.payne@b-next.com>
2018-09-24 11:01:58 +0200

    1

--- src/scoring_helper/src/Komponenten/Canvas.js
+++ src/scoring_helper/src/Komponenten/Canvas.js
@@ -1,21 +1,31 @@
-class MyCircle extends React.Component {
-    constructor(…args) {
-        super(…args);
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+
+
+class Canvas extends React.Component {
+    constructor() {
+        super();
         this.state = { isMouseInside: false};
         this.handleMouseEnter = this.handleMouseEnter.bind(this);
         this.handleMouseLeave = this.handleMouseLeave.bind(this);
     }
+
     handleMouseEnter() {
         this.setState({ isMouseInside: true});
     }
+
     handleMouseLeave() {
         this.setState({ isMouseInside: false});
     }
+
     render() {
         return (
-            <Circle
+            <Rect
                 x={100} y={60} radius={50}
-                fill=”yellow” stroke=”black”
+                fill='yellow' stroke='black'
                 strokeWidth={this.state.isMouseInside ? 5 : 1}
                 onMouseEnter={this.handleMouseEnter}
                 onMouseLeave={this.handleMouseLeave}
@@ -23,3 +33,4 @@ class MyCircle extends React.Component {
         );
     }
 }
+export default Canvas;
