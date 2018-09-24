67eba0d41c8567cff26c145224431bb2f72bc5be
Payne <sean.payne@b-next.com>
2018-09-24 14:01:43 +0200

    1

--- src/scoring_helper/src/Komponenten/Canvas.js
+++ src/scoring_helper/src/Komponenten/Canvas.js
@@ -3,30 +3,17 @@ import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text } from 'react-konva';
 import Konva from 'konva';
 
+//.css
+import "../CSS/canvas.css"
 
 
 class Canvas extends React.Component {
-    constructor() {
-        super();
-        this.state = { isMouseInside: false};
-        this.handleMouseEnter = this.handleMouseEnter.bind(this);
-        this.handleMouseLeave = this.handleMouseLeave.bind(this);
-    }
-
-    handleMouseEnter() {
-        this.setState({ isMouseInside: true});
-    }
-
-    handleMouseLeave() {
-        this.setState({ isMouseInside: false});
-    }
-
     render() {
         return (
+          <div className="canvas">
+            <canvas className="spielfeld"></canvas>
+          </div>
 
-          <div>
-            <canvas ref={canvasHex => this.canvasHex =canvasHex}></canvas>
-            </div>
         );
     }
 }
