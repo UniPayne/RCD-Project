f634f58ff0d25c4ef46c995f24db3c883e8e36a0
Payne <sean.payne@b-next.com>
2018-09-24 14:43:24 +0200

    1

--- src/scoring_helper/src/Komponenten/Canvas.js
+++ src/scoring_helper/src/Komponenten/Canvas.js
@@ -8,12 +8,27 @@ import "../CSS/canvas.css"
 
 
 class Canvas extends React.Component {
+
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+
+    const img = this.refs.image
+    ctx.beginPath();
+    ctx.moveTo(40,16);    //Startpunkt oben in der Mitte
+    ctx.lineTo(16,40);    //Mitte links
+    ctx.lineTo(40,64);    //Unten Mitte
+    ctx.lineTo(64,40);    //Mitte Rechts
+    ctx.lineTo(40,16);    //Zurück nach Oben
+    ctx.strokeStyle = "gray"; //mach die Linie grau
+    ctx.lineWidth = 3;
+    ctx.stroke();
+    ctx.closePath();
+}
+
     render() {
         return (
-          <div className="canvas">
-            <canvas className="spielfeld"></canvas>
-          </div>
-
+            <canvas ref="canvas" className="spielfeld" height="80px" width="80px"></canvas>
         );
     }
 }
